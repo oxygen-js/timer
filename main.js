d6781110d1b069261bb98af0373097791347e27a
a.kazakov <a.kazakov@bidzaar.com>
2021-11-13 20:55:51 +0300

    Fix bug

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 "use strict"
 
 let interval;
+let timer = moment({ h: 0, m: 0, s: 0, ms: 0});
 
-let start = getHtmlById("start");
-let resetBtn = getHtmlById("reset");
 let hoursElement = getHtmlById("hours");
 let minutesElement = getHtmlById("minutes");
 let secondsElement = getHtmlById("seconds");
+
+/** Buttons **/
+let start = getHtmlById("start");
+let resetBtn = getHtmlById("reset");
 let add_1_minutes = getHtmlById("add_minutes");
 let add_5_minutes = getHtmlById("add_5_minutes");
 let add_10_minutes = getHtmlById("add_10_minutes");
 let add_30_minutes = getHtmlById("add_30_minutes");
-
 let add_hour = getHtmlById("add_hour");
-
-let timer = moment({ h: 0, m: 0, s: 0, ms: 0});
 
 add_1_minutes.addEventListener("click", () => {
     timer.add(1, "minutes");
@@ -42,8 +42,25 @@ add_hour.addEventListener("click", () => {
 });
 
 start.addEventListener("click", () => {
+    let hours = timer.hours();
+    let minutes = timer.minutes();
+    let seconds = timer.seconds();
+
+    if (hours === 0 && minutes === 0 && seconds === 0) {
+        return;
+    }
+
+    start.setAttribute("disabled", true);
+    startTimer();
+});
+
+resetBtn.addEventListener("click", () => {
+    stopTimer();
+});
+
+
+function startTimer() {
     interval = setInterval(() => {
-        timer.subtract(1, "seconds");
         let hours = timer.hours();
         let minutes = timer.minutes();
         let seconds = timer.seconds();
@@ -53,27 +70,24 @@ start.addEventListener("click", () => {
             return;
         }
 
+        timer.subtract(1, "seconds");
         setHtml({ hours , minutes, seconds })
     }, 1000);
-});
-
-resetBtn.addEventListener("click", () => {
-    stopTimer();
-})
-
-
-function setHtml({ hours = 0, minutes = 0, seconds = 0 }) {
-    hoursElement.textContent = getConvertTime(hours);
-    minutesElement.textContent = getConvertTime(minutes);
-    secondsElement.textContent = seconds < 10 ? `0${seconds}` : `${seconds}`;
 }
 
 function stopTimer() {
+    start.setAttribute("disabled", false);
     clearInterval(interval);
     timer.hours(0);
     timer.minutes(0);
     timer.seconds(0);
     setHtml({ hours: timer.hours(), minute: timer.minutes(), seconds: timer.seconds() });
+}
+
+function setHtml({ hours = 0, minutes = 0, seconds = 0 }) {
+    hoursElement.textContent = getConvertTime(hours);
+    minutesElement.textContent = getConvertTime(minutes);
+    secondsElement.textContent = seconds < 10 ? `0${seconds}` : `${seconds}`;
 }
 
 function getHtmlById(id) {
